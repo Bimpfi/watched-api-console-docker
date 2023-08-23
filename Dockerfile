c694ec2b454b372492c9eb2e93d77e695c25c344
Hülß <mar6067m@hs-coburg.de>
2023-08-23 08:49:09 +0200

    remove everything for update

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:12.13.1-alpine
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-RUN npm install -g nodemon
-COPY . .
-# EXPOSE 8000
-CMD [ "sh", "-c", "nodemon --inspect -V ./model.js & node index.js"]
--- model.js
+++
@@ -1,7 +0,0 @@
-const generator = require('@api-components/api-model-generator');
-generator('./apis.json', {
-  dest: './models/',
-  src: './'
-})
-  .then(() => console.log('Models created'))
-  .catch((cause) => console.error(cause));
